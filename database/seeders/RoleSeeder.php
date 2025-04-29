346abb31f58d92c0bc12c5d79c9344cc64d5ac83
APPOM <>
2025-04-29 11:58:00 +0800

    bahu

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -2,8 +2,8 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;  // <-- Import DB facade here
 
 class RoleSeeder extends Seeder
 {
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,8 +2,9 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
-use Illuminate\Database\Seeder;
+use Illuminate\Database\Seeder;  // Correct order
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;  // Correct the spelling here
 
 class UserSeeder extends Seeder
 {
@@ -14,17 +15,19 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-      user::insert([
-        'name' => 'admin',
-        'email' => 'admin@gmail.com',
-        'password' => hash::make('111'),
-        'role_id' => 1,
-      ],
-      [
-        'name' => 'user',
-        'email' => 'user@gmail.com',
-        'password' => hash::make('111'),
-        'role_id' => 2,
-      ]);
+        User::insert([
+            [
+                'name' => 'admin',
+                'email' => 'admin@gmail.com',
+                'password' => Hash::make('111'),
+                'role_id' => 1,
+            ],
+            [
+                'name' => 'user',
+                'email' => 'user@gmail.com',
+                'password' => Hash::make('111'),
+                'role_id' => 2,
+            ],
+        ]);
     }
 }
